59675c78d40e30dd845754a12b02e79424f50a56
ikeraguero <ikerpires407@gmail.com>
2023-10-14 15:22:03 -0300

    Add Mobile Nav Functionality

--- omnifood/js/script.js
+++ omnifood/js/script.js
@@ -1,7 +1,18 @@
+// Set current year
+
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 console.log(currentYear);
 yearEl.textContent = currentYear;
+
+// Mobile Nav
+
+const btnNavEl = document.querySelector(".nav-menu-mobile");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
